f05e40b2da0422b0e40ebff9c475f5af089a8e67
James Gatannah <james.gatannah@gmail.com>
2016-12-26 02:48:17 +0000

    WIP Slap together a more generic protocol builder
    Well, the easy parts of the client half.
    
    From this perspective, it looks pretty simple/easy/elegant.
    
    Time will tell.

--- src/com/frereth/common/protocol.clj
+++ src/com/frereth/common/protocol.clj
@@ -1,62 +1,107 @@
 (ns com.frereth.common.protocol
   "Centralize the communications hand-shake layer"
-  (:require [manifold.deferred :as d]
+  (:require [clojure.spec :as s]
+            [manifold.deferred :as d]
             [manifold.stream :as stream])
   (:import clojure.lang.ExceptionInfo))
 
-(defmulti -step-builder
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Specs
+
+(defmulti -client-step-builder
   "Build an individual step handler in a protocol chain"
-  ::operation)
+  (fn [_ _ dscr]
+    (::direction dscr)))
+
+(defmulti -server-step-builder
+  "Build an individual step handler in a protocol chain"
+  (fn [_ _ dscr]
+    (::direction dscr)))
+
+(s/def ::version (s/or :ints (s/tuple int? int? int?)
+                       :build-string (s/tuple int? int? string?)
+                       :strings (s/tuple string? string? string?)
+                       :string string?))
+
+(s/def ::versions (s/coll-of ::version))
+
+;; Keyword names of protocols and the supported versions
+;; Note that, really, this should specifically be namespaced keywords
+(s/def ::protocol-versions (s/map-of keyword? ::versions))
+
+(s/def ::icanhaz
+  ;; Note that this is really making a request for some sort of resource.
+  ;; In this initial example, the server lists which further protocol versions
+  ;; it supports, requesting the server to pick one.
+  (s/tuple #(= % ::icanhaz) (s/keys :req [::protocol-versions])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Internal Implementation
+
+(defmethod -client-step-builder ::client->server
+  [strm timeout dscr]
+  (fn [_]
+    (let [spec (::spec dscr)
+          msg "This needs to be generated, somehow"]
+      (stream/try-put! strm msg timeout ::timeout))))
+
+(defmethod -client-step-builder ::server->client
+  [strm timeout dscr]
+  [(fn [sent]
+     (if (not= sent ::timeout)
+       (stream/try-take! strm ::drained timeout ::timeout)))
+   (fn [response]
+     (let [spec (::spec dscr)]
+       (when-not (s/valid? spec response)
+         (throw (ex-info (::problem dscr) {:problem (s/explain spec response)})))))])
 
 (defn protocol-agreement
-  [strm msgs timeout]
-  (-> strm
-      (d/chain (map -step-builder msgs))
-      (d/catch ExceptionInfo #(println "Whoops:" %))
-      (d/catch RuntimeException #(println "How'd I miss:" %))
-      (d/catch Exception #(println "Major oops:" %))))
+  [strm builder timeout step-descriptions]
+  (let [steps (mapcat (partial builder strm timeout)
+                      step-descriptions)]
+    (-> strm
+        (d/chain steps)
+        (d/catch ExceptionInfo #(println "Whoops:" %))
+        (d/catch RuntimeException #(println "How'd I miss:" %))
+        (d/catch Exception #(println "Major oops:" %)))))
 
-(defn client->server
-  "This really seems like it should be a higher-order handler coping with arbitrary handler chains.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Public
+;;;; Actually, these probably shouldn't be public in here.
+;;;; They belong in a different ns that exposes the protocol building
+;;;; pieces so another ns can combine these with the actual protocol
+;;;; definitions.
 
-Note that this is pretty deliberately insecure."
-  [strm timeout versions]
-  (-> strm
-      (d/chain
-       #(stream/try-put! % :ohai timeout ::timeout)
-       (fn [sent]
-         (if (not= sent ::timeout)
-           (stream/try-take! strm ::drained timeout ::timeout)
-           (throw (ex-info "Initial connection timed out" {}))))
-       (fn [token]
-         (when (not= :orly? token)
-           (throw (ex-info "Broken handshake"))))
-       (fn [_]
-         (stream/try-put! strm (list 'icanhaz? versions) timeout ::timeout))
-       (fn [sent]
-         (if (not= sent ::timeout)
-           (stream/try-take! strm ::drained timeout ::timeout)
-           (throw (ex-info "Version negotiation timed out" {}))))
-       (fn [version]
-         ))
-      (d/catch ExceptionInfo))
-  (async/go
-    (let [t-o (async/timeout timeout)]
-      (async/alt!
-        [[ch :ohai!]] ([_ _]
-                       )
-        timeout :timed-out))))
+;;;; Then again, this is small/simple enough that that probably isn't
+;;;; worth the effort
 
-;; So, if I were writing a higher-level version of that, what would it look like?
-(defn protocol-agreement
-  [strm timeout versions]
-  (protocol-exchange strm
-                     [{::operation ::put!
-                       ::value :ohai}
-                      {::operation ::take!
-                       ::expected :orly?
-                       ::problem "Broken handshake"}
-                      {::operation ::put!
-                       ::value (list 'icanhaz versions)}
-                      {::operation ::take!}]
-                     timeout))
+(defn client-protocol-agreement
+  "Generates the client-side protocol handler"
+  [strm timeout msgs]
+  (protocol-agreement strm -client-step-builder timeout msgs))
+
+(defn server-protocol-agreement
+  "Generates the server-side of a protocol handler"
+  [strm timeout msgs]
+  (protocol-agreement strm -server-step-builder timeout msgs))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Protocols
+;;;; These really do belong in here. The rest should probably go into a
+;;;; nested impl ns
+
+(defn version-contract
+  "Declaration of the handshake to allow client and server to agree on handling the next pieces"
+  []
+  [{::direction ::client->server
+    ::spec #(= % ::ohai)
+    ::problem "Illegal greeting"}
+   {::direction ::server->client
+    ::spec #(= % ::orly?)
+    ::problem "Broken handshake"}
+   {::direction ::client->server
+    ::spec ::icanhaz}
+   {::direction ::server->client
+    ::spec (s/or :match (s/and ::protocol-versions
+                               #(= 1 (count %)))
+                 :fail #(= % ::lolz))}])
